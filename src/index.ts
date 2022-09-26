f9c42af978ca8ba6b3dbc21de3a7164c36fffc53
anubhab-patnaik <anubhab.patnaik@squbix.com>
2022-09-26 13:46:35 +0530

    removing test checks

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export function printfromindex(){
-		console.log("Hello World");
-}
-printfromindex();
-export const str: string = "Hello World";
-module.exports= {
-	printfromindex,
-	str,
-}
--- test/index.ts
+++
@@ -1,5 +0,0 @@
-import { printfromindex } from "../src/index";
-import {str} from "../src/index";
-console.log("Logging from test/index.ts");
-printfromindex();
-console.log(str);
