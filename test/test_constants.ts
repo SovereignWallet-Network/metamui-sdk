efde70bd161d9a1245bd05a856d4a41e21362629
anubhab-patnaik <anubhab.patnaik@squbix.com>
2022-09-28 16:17:02 +0530

    updating testconstants to use swn

--- test/test_constants.ts
+++ test/test_constants.ts
@@ -1,7 +1,7 @@
 const validSchema = '0x15733EA25E1C3A1A8F2330D1B64E80B765971A370653943DB57A2099EF0EDF1F';
 const inValidSchema = '0x15733EA25E1C3A1A8F2330D1B64E80B765971A370653943DB57A2099EF0EDF1G';
-const mnemonicWithBalance = '//Alice'; // this mnemonic should have balance for test to pass
-const didOfMnemonicWithBalance = 'Alice'; // this should match chain data
+const mnemonicWithBalance = '//Swn'; // this mnemonic should have balance for test to pass
+const didOfMnemonicWithBalance = 'Swn'; // this should match chain data
 const providerNetwork = process.env.PROVIDER_NETWORK || 'testnet';
 
 export {
@@ -11,3 +11,7 @@ export {
   didOfMnemonicWithBalance,
   providerNetwork,
 };
+
+//cheese middle hub gallery video leg category prepare casual eternal true riot
+//mother umbrella ramp dash vivid police language twenty fiber chimney enough menu
+//about ivory online usual point nominee envelope warrior rhythm text boring toy
