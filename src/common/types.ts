2950b411d60bfbc9878acde8df1da6d662b9d387
anubhab-patnaik <anubhab.patnaik@squbix.com>
2022-10-12 10:12:00 +0530

    added vcmodule: errors pending

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,12 +1,12 @@
 
 type PRIVATE_DID_TYPE =
-{ 
-    private: 
-    { 
-        public_key: Uint8Array;
-        identity: String;
-        metadata: String;
-    } 
-};
+    {
+        private:
+        {
+            public_key: Uint8Array | undefined;
+            identity: String;
+            metadata: String;
+        }
+    };
 
 export { PRIVATE_DID_TYPE };
