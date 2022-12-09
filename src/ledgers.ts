f5a4849834d86f9950accc8580953cc43e9237f9
sumeetsqubix <sumeet.naik@squbix.com>
2022-12-09 12:12:02 +0530

    Add storage query functions to ledgers module

--- src/ledgers.ts
+++ src/ledgers.ts
@@ -6,6 +6,8 @@ import { submitTransaction } from './common/helper';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { encodeData, CURRENCY_CODE_BYTES } from './utils';
 
+// Extrinsic functions
+
 /**
  * Issue a new currency
  * @param {HexString} vcId
@@ -237,10 +239,136 @@ async function slashToken(
  * @param {String} token
  * @returns {String} Sanitised Token Name
  */
- const sanitiseCCode = (token: String): String => {
+ const sanitiseCCode = (token) => {
+   if(!token) return null;
    if (token.startsWith('0x'))
       return token.padEnd(CURRENCY_CODE_BYTES, '\0');
    return encodeData(token.padEnd(CURRENCY_CODE_BYTES, '\0'), 'currency_code');
+}
+
+// Storage Query Functions
+
+/**
+ * Get the token balance of an account
+ * @param {String} currencyCode
+ * @param {String} did
+ * @param {ApiPromise} api
+ */
+ async function accounts(
+   currencyCode: String,
+   did: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.accounts(sanitiseCCode(currencyCode), sanitiseDid(did))).toJSON();
+}
+
+/**
+ * Get any liquidity locks of a token type under an account
+ * @param {String} currencyCode
+ * @param {String} did
+ * @param {ApiPromise} api
+ */
+ async function locks(
+   currencyCode: String,
+   did: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.locks(sanitiseCCode(currencyCode), sanitiseDid(did))).toJSON();
+}
+
+/**
+ * Storage map between currency code and block number 
+ * @param {ApiPromise} api
+ * @param {String} currencyCode (Optional)
+ */
+ async function removedTokens(
+   api: ApiPromise,
+   currencyCode?: String,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.removedTokens(sanitiseCCode(currencyCode))).toJSON();
+}
+
+/**
+ * Token currency counter
+ * @param {ApiPromise} api
+ */
+ async function tokenCurrencyCounter(
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.tokenCurrencyCounter()).toString();
+}
+
+/**
+ * Map to store a friendly token name for token
+ * @param {String} currencyCode
+ * @param {ApiPromise} api
+ */
+ async function tokenData(
+   currencyCode: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.tokenData(sanitiseCCode(currencyCode))).toJSON();
+}
+
+/**
+ * Get Token Information
+ * @param {String} currencyCode
+ * @param {ApiPromise} api
+ * @returns {Number} Currency Id
+ */
+async function tokenInfo(
+   currencyCode: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.tokenInfo(sanitiseCCode(currencyCode))).toString();
+}
+
+/**
+ * Reverse lookup Token Information
+ * @param {Number} currencyId
+ * @param {ApiPromise} api
+ * @returns {HexString} Currency Code Hex
+ */
+ async function tokenInfoRLookup(
+   currencyId: Number,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.tokenInfoRLookup(currencyId)).toHex();
+}
+
+/**
+ * Lookup Token Issuer
+ * @param {String} currencyCode
+ * @param {ApiPromise} api
+ * @returns {HexString} Token Owner DID Hex
+ */
+ async function tokenIssuer(
+   currencyCode: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.tokenIssuer(sanitiseCCode(currencyCode))).toHex();
+}
+
+/**
+ * Get Total Token Issuance
+ * @param {String} currencyCode
+ * @param {ApiPromise} api
+ * @returns {Number} Token Issuance
+ */
+async function totalIssuance(
+   currencyCode: String,
+   api: ApiPromise,
+  ) {
+   const provider = api || (await buildConnection('local'));
+   return (await provider.query.tokens.totalIssuance(sanitiseCCode(currencyCode))).toString();
 }
 
 export {
@@ -253,5 +381,14 @@ export {
    transferAll,
    transferTokenWithMemo,
    transferToken,
-   sanitiseCCode
+   sanitiseCCode,
+   accounts,
+   locks,
+   removedTokens,
+   tokenCurrencyCounter,
+   tokenData,
+   tokenInfo,
+   tokenInfoRLookup,
+   tokenIssuer,
+   totalIssuance
 };
